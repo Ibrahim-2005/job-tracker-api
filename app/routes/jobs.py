b0e8de1d2dbda0a415d44d33051927180969791a
MOHAMED IBRAHIM <ibrahimchennai2005@gmail.com>
2026-04-11 15:30:43 +0530

    Functions: get_history of status and saves the history of a job's status in StatusHistory database and reads it

--- app/routes/jobs.py
+++ app/routes/jobs.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.models.job import Job
+from app.models.status_history import StatusHistory
 from app import db
 from datetime import datetime,timezone
 
@@ -50,7 +51,15 @@ def update_job(job_id):
     
     job.company=data.get('company',job.company)
     job.role=data.get('role',job.role)
-    job.status=data.get('status',job.status)
+    if 'status' in data and data['status'] != job.status:
+        history = StatusHistory(
+            job_id=job.id,
+            from_status=job.status,
+            to_status=data['status'],
+            changed_at=datetime.now(timezone.utc)
+        )
+        db.session.add(history)
+        job.status = data['status']
 
     db.session.commit()
     return jsonify({"msg": "Job updated"})
@@ -68,3 +77,25 @@ def delete_job(job_id):
     job.deleted_at=datetime.now(timezone.utc)
     db.session.commit()
     return jsonify({"msg":"Job Deleted"})
+
+@jobs_bp.route('/jobs/<int:job_id>/history', methods=['GET'])
+@jwt_required()
+@jobs_bp.route('/jobs/<int:job_id>/history', methods=['GET'])
+@jwt_required()
+def get_history(job_id):
+    user_id = int(get_jwt_identity())
+
+    job = Job.query.filter_by(id=job_id,user_id=user_id,deleted_at=None).first()
+
+    if not job:
+        return jsonify({"error": "Job not found"}), 404
+
+    history = StatusHistory.query.filter_by(job_id=job_id).all()
+
+    return jsonify([
+        {
+            "from": h.from_status,
+            "to": h.to_status,
+            "time": h.changed_at
+        } for h in history
+    ])
