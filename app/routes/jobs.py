418fe232efc2cdb9c4b8ae8ef7c7de75f5f016ef
MOHAMED IBRAHIM <ibrahimchennai2005@gmail.com>
2026-04-11 10:58:32 +0530

    Functions: update_job and delete_job added

--- app/routes/jobs.py
+++ app/routes/jobs.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.models.job import Job
 from app import db
+from datetime import datetime,timezone
 
 jobs_bp = Blueprint('jobs', __name__)
 
@@ -25,7 +26,7 @@ def create_job():
 @jwt_required()
 def get_jobs():
     user_id=get_jwt_identity()
-    jobs=Job.query.filter_by(user_id=user_id).all()
+    jobs=Job.query.filter_by(user_id=user_id,deleted_at=None).all()
 
     return jsonify([
         {
@@ -36,3 +37,34 @@ def get_jobs():
 
         }for j in jobs
     ])
+
+@jobs_bp.route('/jobs/<int:job_id>',methods=['PUT'])
+@jwt_required()
+def update_job(job_id):
+    user_id=int(get_jwt_identity())
+    data=request.get_json()
+
+    job=Job.query.filter_by(id=job_id,user_id=user_id,deleted_at=None).first()
+    if not job or not job.company or not job.role or not job.status:
+        return jsonify({"msg":"Job not found"}),404
+    
+    job.company=data.get('company',job.company)
+    job.role=data.get('role',job.role)
+    job.status=data.get('status',job.status)
+
+    db.session.commit()
+    return jsonify({"msg": "Job updated"})
+
+@jobs_bp.route('/jobs/<int:job_id>',methods=['DELETE'])
+@jwt_required()
+def delete_job(job_id):
+    user_id=get_jwt_identity()
+    
+    job=Job.query.filter_by(id=job_id,user_id=user_id).first()
+
+    if not job:
+        return jsonify({"msg":"Job not found"}),404
+    
+    job.deleted_at=datetime.now(timezone.utc)
+    db.session.commit()
+    return jsonify({"msg":"Job Deleted"})
