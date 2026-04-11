702a99675386e1d1414e193ec2477234f5609e07
MOHAMED IBRAHIM <ibrahimchennai2005@gmail.com>
2026-04-11 15:48:52 +0530

    Made timezone clear and created foreign key for job and status_history tables

--- app/models/job.py
+++ app/models/job.py
@@ -3,7 +3,7 @@ from datetime import datetime,timezone
 
 class Job(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     company = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(255), nullable=False)
     status = db.Column(db.String(50), default='applied')
@@ -11,4 +11,4 @@ class Job(db.Model):
     job_url = db.Column(db.String(500))
     applied_date = db.Column(db.Date)
     deleted_at = db.Column(db.DateTime)
-    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    created_at = db.Column(db.DateTime(timezone=True),default=lambda: datetime.now(timezone.utc))
--- app/models/status_history.py
+++ app/models/status_history.py
@@ -3,8 +3,8 @@ from datetime import datetime,timezone
 
 class StatusHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    job_id = db.Column(db.Integer, nullable=False)
+    job_id = db.Column(db.Integer, db.ForeignKey('job.id'), nullable=False)
     from_status = db.Column(db.String(50))
     to_status = db.Column(db.String(50), nullable=False)
-    changed_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    changed_at = db.Column(db.DateTime(timezone=True),default=lambda: datetime.now(timezone.utc))
     note = db.Column(db.Text)
